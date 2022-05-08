0b895e11dd43f84e73083d396c0f0bcd33b30a94
Fabian Skarmeta <fabian.skarmeta@gmail.com>
2022-05-08 23:29:37 +0200

    ADD: test

--- config/plugins.js
+++ config/plugins.js
@@ -16,7 +16,7 @@ module.exports = ({ env }) => ({
   graphql: {
     config: {
       endpoint: "/graphql",
-      shadowCRUD: true,
+      shadowCRUD: false,
       playgroundAlways: true,
       depthLimit: 7,
       amountLimit: 100,
